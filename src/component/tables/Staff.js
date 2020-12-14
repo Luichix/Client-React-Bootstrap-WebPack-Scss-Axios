019249ffc510563baec32f3e119be0fa5f19a648
Luichix <luigui.rex@gmail.com>
2020-12-14 16:15:50 -0600

    Generating recuitment form

--- src/component/tables/Staff.js
+++ src/component/tables/Staff.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { Container, Table, Button, Modal } from 'react-bootstrap'
 import Recruitment from '../form/Recruitment'
+
 function MyVerticallyCenteredModal (props) {
   return (
       <Modal
@@ -9,7 +10,6 @@ function MyVerticallyCenteredModal (props) {
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered
-        onHide="false"
       >
         <Modal.Header >
           <Modal.Title id="contained-modal-title-vcenter" >
@@ -26,6 +26,11 @@ function MyVerticallyCenteredModal (props) {
   )
 }
 
+const animeflv = [
+  { id: 1, personaje: 'Naruto', anime: 'Naruto' },
+  { id: 2, personaje: 'Goku', anime: 'Dragon Ball' }
+]
+
 const Staff = () => {
   const [modalShow, setModalShow] = useState(false)
 
@@ -34,107 +39,20 @@ const Staff = () => {
 <Table striped bordered hover>
   <thead>
     <tr>
-      <th>#</th>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-      <th>Username</th>
-      <th>Last Name</th>
-      <th>Username</th>
-      <th>Username</th>
+      <th>Id</th>
+      <th>Personaje</th>
+      <th>Anime</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <td>2</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>@mdo</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-    </tr>
+    {animeflv.map((elemento) => (
+      <tr key={elemento.id} >
+        <td>{elemento.id}</td>
+        <td>{elemento.personaje}</td>
+        <td>{elemento.anime}</td>
+      </tr>
+    ))}
+
   </tbody>
 </Table>
     <Button variant="primary" onClick={() => setModalShow(true)}>
