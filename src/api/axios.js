92856e915a2931be82f5ab8fb66195b7e58d85e5
Luichix <luigui.rex@gmail.com>
2020-12-14 20:40:56 -0600

    config url api axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.13:8081/',
+  baseURL: 'http://localhost:8081/',
   headers: { 'Content-type': 'application/json' }
 })
 
